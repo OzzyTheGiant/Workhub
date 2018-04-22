6ac8be46e31d03061ec491a552f0799cd4ec4ebc
Oziel Perez <OZLperez11@gmail.com>
2018-04-22 17:56:06 -0500

    Added configuration for Hibernate, MySQL connector, and JPA repositories

--- src/main/java/dreamcraft/workhub/Application.java
+++ src/main/java/dreamcraft/workhub/Application.java
@@ -1,4 +1,15 @@
 package dreamcraft.workhub;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableAutoConfiguration
+@EnableJpaRepositories("dreamcraft.workhub.dao")
+@ComponentScan(basePackages = "dreamcraft.workhub")
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
---
+++ src/main/java/dreamcraft/workhub/config/DataConfig.java
@@ -0,0 +1,64 @@
+package dreamcraft.workhub.config;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class DataConfig {
+    @Autowired private Environment env;
+
+//    @Bean
+//    public LocalSessionFactoryBean createSessionFactory() {
+//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//        sessionFactory.setDataSource(createDataSource());
+//        sessionFactory.setPackagesToScan("dreamcraft.workhub.model");
+//        sessionFactory.setHibernateProperties(getHibernateProperties());
+//        return sessionFactory;
+//    }
+
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean createEntityManager() {
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(createDataSource());
+        entityManager.setPackagesToScan(env.getProperty("entityManager.packagesToScan"));
+        entityManager.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        entityManager.setJpaProperties(getHibernateProperties());
+        return entityManager;
+    }
+
+    @Bean
+    public DataSource createDataSource() {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(env.getProperty("db.url")); // contains database name, otherwise set via .setDatabaseName()
+        dataSource.setUser(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager createTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(createEntityManager().getObject());
+        return transactionManager;
+    }
+
+    @Bean
+    public Properties getHibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        hibernateProperties.setProperty("hibernate.ddl-auto", env.getProperty("hibernate.ddl-auto"));
+        hibernateProperties.setProperty("hibernate.show_sql", env.getProperty("hibernate.showSQL"));
+        return hibernateProperties;
+    }
+}
