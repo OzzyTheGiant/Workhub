1954504821b3c5f35280664b859cd6d6bdacd9db
Oziel Perez <OZLperez11@gmail.com>
2018-05-07 16:56:45 -0500

    Created Header DOM component with styles

---
+++ react/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header id="Header">
+            <img src="http://placehold.it/120x35" alt="Workhub Logo"/>
+            <nav>
+                <ul>
+                    <li>Documents</li>
+                    <li>Log Out</li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
--- react/src/index.js
+++ react/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'normalize.css';
+import 'css/main-styles.css';
+import App from 'components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('react'));
 registerServiceWorker();
