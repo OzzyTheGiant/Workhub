f06c47f1b3717b555bf3a407b50266c003336e94
Oziel Perez <OZLperez11@gmail.com>
2018-07-03 09:14:39 -0500

    Added tomcat as provided runtime and configured app accordingly

--- src/main/java/dreamcraft/workhub/Application.java
+++ src/main/java/dreamcraft/workhub/Application.java
@@ -2,12 +2,20 @@ package dreamcraft.workhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 }
