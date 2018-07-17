3ad36951d5d69ec56d594579d920835b05fc2ce0
Oziel Perez <OZLperez11@gmail.com>
2018-07-17 15:54:03 -0500

    Changed folder structure for Descriptive type

--- src/main/java/dreamcraft/workhub/filesystem/DescriptiveFolderStructure.java
+++ src/main/java/dreamcraft/workhub/filesystem/DescriptiveFolderStructure.java
@@ -6,16 +6,16 @@ public class DescriptiveFolderStructure implements FolderStructure {
     @Override
     public String generateFilePath(Document doc) {
         return String.format(
-                "/[%s] %s/[%s] %s/[%s][%s][%d] %s%s",
-                doc.getClient().getId(),
-                doc.getClient().getClientName(),
-                doc.getProject().getCategory().getDescription(),
-                doc.getProject().getName(),
-                doc.getId(),
-                doc.getCategory().getDescription(),
-                doc.getYear(),
-                doc.getDescription(),
-                doc.getFileType().getExtension()
+			"/%s [%s]/[%s] %s/[%s][%d][%s] %s%s",
+			doc.getClient().getClientName(),
+			doc.getClient().getId(),
+			doc.getProject().getCategory().getDescription(),
+			doc.getProject().getName(),
+			doc.getCategory().getDescription(),
+			doc.getYear(),
+			doc.getId(),
+			doc.getDescription(),
+			doc.getFileType().getExtension()
         );
     }
  }
